5c53415df5ce051adac203479bb51bd19a7afb43
Reuben Ramos <rsramos23@gmail.com>
2023-10-16 22:58:06 -0700

    step 1 done

---
+++ js/01-functions-practice.js
@@ -0,0 +1,16 @@
+//STEP 1
+
+const halfNumber = num => {
+    num = prompt("Please input a number")
+    answer = num/2
+    console.log(`Half of ${num} is ${answer}`)
+}
+halfNumber()
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
