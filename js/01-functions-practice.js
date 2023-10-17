6092c3e027e8d2baffdee09b4155fbf142720814
Reuben Ramos <rsramos23@gmail.com>
2023-10-16 23:23:30 -0700

    step 3 fp done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -9,14 +9,20 @@
 
 //STEP 2
 
-const squareNumber = num => {
-    num = prompt("Please input a number to square")
-    answer = num**2
-    console.log(`The result of squaring the number ${num} is ${answer}`)
-}
-squareNumber()
+// const squareNumber = num => {
+//     num = prompt("Please input a number to square")
+//     answer = num**2
+//     console.log(`The result of squaring the number ${num} is ${answer}`)
+// }
+// squareNumber()
 //STEP 3
-
+const percentOf =  (num1, num2) => {
+    num1 = prompt("choose a number")
+    num2 = prompt("Choose a number")
+    let percent = num1/num2 *100
+    console.log(`${num1} is ${percent}% of ${num2}`)
+}
+percentOf()
 //STEP 4
 
 //STEP 5
