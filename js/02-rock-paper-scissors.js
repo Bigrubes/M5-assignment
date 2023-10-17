bb04e76d2f860420469b03991e6789d345c5a9b2
Reuben Ramos <rsramos23@gmail.com>
2023-10-17 10:42:11 -0700

    basic calculator added switch statement

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,26 +1,40 @@
 const rps = (user,computer) =>{
-    user = prompt("please choose rock paper or scissors")
+    user = prompt("Let's play Rock, Paper Scissors! Rock, Paper, Scissors. Shoot!")
+  
     console.log(user)
-    rock = "rock"
-    paper = "paper"
-    scissors = "scissors"
+    // rock = "rock"
+    // paper = "paper"
+    // scissors = "scissors"
+    if (user !== "rock" && user !== "paper" && user !== "scissors"){
+        alert('You must type "rock", "paper" or "scissors".')
+    }
     computer = Math.round(Math.random()*2)
+    if (computer===1){
+        parseInt(computer==="paper")
+    }else if(computer===2){
+        parseInt(computer==="scissors")
+    }else{(computer===0)
+        parseInt(computer==="rock")
+
+    }
     console.log(computer)
 
-    if (user=== rock && computer===0 || user === paper && computer===1 || user===scissors && computer===2){
-        document.write("Tie game you chose the same as the computer")
-    }else if(user===rock && computer ===1){
-        document.write(`You chose ${user} and the computer chose paper. You Lose!`)
-    } else if(user===rock && computer===2){
-        document.write(`You chose ${user} and the computer chose scissors. You Win!`)
-    } else if(user===paper && computer===2){
-        document.write(`You chose ${user} and the computer chose scissors. You Lose!`)
-    } else if(user===paper && computer===0){
-        document.write(`You chose ${user} and the computer chose rock. You Win!`)
-    } else if(user===scissors && computer===1){
-        document.write(`You chose ${user} and the computer chose paper. You Win!`)
-    } else{(user===scissors && computer===0)
-        document.write(`You chose ${user} and the computer chose rock. You Lose!`)
+    // if (user=== rock && computer===0 || user === paper && computer===1 || user===scissors && computer===2){
+    //     alert("Tie game you chose the same as the computer")
+    if(user===computer){
+        alert("You chose the same as the computer")
+    } if(user==="rock" && computer ==="paper"){
+        alert(`You chose ${user} and the computer chose paper. You Lose!`)
+    }  if(user==="rock" && computer==="scissors"){
+        alert(`You chose ${user} and the computer chose scissors. You Win!`)
+    }  if(user==="paper" && computer==="scissors"){
+        alert(`You chose ${user} and the computer chose scissors. You Lose!`)
+    }  if(user==="paper" && computer==="rock"){
+        alert(`You chose ${user} and the computer chose rock. You Win!`)
+    }  if(user==="scissors" && computer==="paper"){
+        alert(`You chose ${user} and the computer chose paper. You Win!`)
+    }  if(user==="scissors" && computer==="rock"){
+        alert(`You chose ${user} and the computer chose rock. You Lose!`)
     }
 }
 rps()
--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,9 +1,44 @@
 // ADD A FUNCTION CALLED CALCULATE
+const calculate = (num1,num2, operation, equation) => {
+
 
 // COLLECT FIRST NUMBER FROM USER
-
+num1 = prompt("Enter first number in calculation")
+console.log(num1)
 // COLLECT SECOND NUMBER FROM USER
-
+num2 = prompt("Enter second number in calculation")
+console.log(num2)
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
-
+// operation = prompt("Enter an operation (+,-,*,/")
 // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+do{
+    operation = prompt('You must input "+", "-", "*" or "/"')
+
+}while(operation !== "+" && operation !== "-" && operation !== "*" && operation !== "/")
+
+// if (operation==="*"){
+//     alert(num1*num2)
+// }
+// else if(operation==="-"){
+//     alert(num1 - num2)
+// }
+// else if(operation==="/"){
+//     alert(num1/num2)
+// }else{
+//     alert(parseInt(num1) + parseInt(num2))
+// }
+switch(operation){
+    case "*":
+        alert(num1*num2)
+        break
+    case "-":
+        alert(num1 - num2)
+        break
+    case  "/":
+        alert(num1/num2)
+        break
+    case "+":
+        alert(parseInt(num1) + parseInt(num2))
+}
+}
+calculate()
