41f6ed55be20fc9a5ff421b0be72bbb3542297f2
Reuben Ramos <rsramos23@gmail.com>
2023-10-16 23:07:56 -0700

    step 2 fp done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,14 +1,20 @@
 //STEP 1
 
-const halfNumber = num => {
-    num = prompt("Please input a number")
-    answer = num/2
-    console.log(`Half of ${num} is ${answer}`)
-}
-halfNumber()
+// const halfNumber = num => {
+//     num = prompt("Please input a number to divide in half")
+//     answer = num/2
+//     console.log(`Half of ${num} is ${answer}`)
+// }
+// halfNumber()
 
 //STEP 2
 
+const squareNumber = num => {
+    num = prompt("Please input a number to square")
+    answer = num**2
+    console.log(`The result of squaring the number ${num} is ${answer}`)
+}
+squareNumber()
 //STEP 3
 
 //STEP 4
