b90f961d8350439c5366149f65945d9de02a0121
Reuben Ramos <rsramos23@gmail.com>
2023-10-16 23:47:27 -0700

    step 4 fp done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -2,8 +2,8 @@
 
 // const halfNumber = num => {
 //     num = prompt("Please input a number to divide in half")
-//     answer = num/2
-//     console.log(`Half of ${num} is ${answer}`)
+//     divide = num/2
+//     console.log(`Half of ${num} is ${divide}`)
 // }
 // halfNumber()
 
@@ -11,18 +11,25 @@
 
 // const squareNumber = num => {
 //     num = prompt("Please input a number to square")
-//     answer = num**2
-//     console.log(`The result of squaring the number ${num} is ${answer}`)
+//     square = num**2
+//     console.log(`The result of squaring the number ${num} is ${square}`)
 // }
 // squareNumber()
 //STEP 3
-const percentOf =  (num1, num2) => {
-    num1 = prompt("choose a number")
-    num2 = prompt("Choose a number")
-    let percent = num1/num2 *100
-    console.log(`${num1} is ${percent}% of ${num2}`)
-}
-percentOf()
+// const percentOf =  (num1, num2) => {
+//     num1 = prompt("choose a number")
+//     num2 = prompt("Choose a number")
+//     let percent = num1/num2 *100
+//     console.log(`${num1} is ${percent}% of ${num2}`)
+// }
+// percentOf()
 //STEP 4
+const findModulus = (num1, num2) =>{
+    num1 = prompt("input number 1")
+    num2 = prompt("input number 2")
+    modulus = num1%num2
+    console.log(`${modulus} is the modulus of ${num1} and ${num2}`)
 
+}
+findModulus()
 //STEP 5
