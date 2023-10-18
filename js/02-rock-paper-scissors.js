c7168ca2432ac5fe987a2566204af7957b2963eb
Reuben Ramos <rsramos23@gmail.com>
2023-10-17 20:52:00 -0700

    rps complete

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,40 +1,39 @@
-const rps = (user,computer) =>{
+const rps = (computer) =>{
     user = prompt("Let's play Rock, Paper Scissors! Rock, Paper, Scissors. Shoot!")
   
     console.log(user)
-    // rock = "rock"
-    // paper = "paper"
-    // scissors = "scissors"
-    if (user !== "rock" && user !== "paper" && user !== "scissors"){
-        alert('You must type "rock", "paper" or "scissors".')
+    while (user !== "rock" && user !== "paper" && user !== "scissors"){
+        alert('You must type "rock", "paper" or "scissors" to play.')
+        user = prompt("Let's play Rock, Paper Scissors! Rock, Paper, Scissors. Shoot!")
     }
-    computer = Math.round(Math.random()*2)
-    if (computer===1){
-        parseInt(computer==="paper")
-    }else if(computer===2){
-        parseInt(computer==="scissors")
-    }else{(computer===0)
-        parseInt(computer==="rock")
 
-    }
-    console.log(computer)
+      const  num = Math.round(Math.random()*3+1)
+        switch (num){
+            case 1:
+                computer="rock"
+                break
+            case 2:
+                computer="paper"
+                break
+            case 3:
+                computer="scissors"
+                break
+                }  
 
-    // if (user=== rock && computer===0 || user === paper && computer===1 || user===scissors && computer===2){
-    //     alert("Tie game you chose the same as the computer")
-    if(user===computer){
-        alert("You chose the same as the computer")
-    } if(user==="rock" && computer ==="paper"){
-        alert(`You chose ${user} and the computer chose paper. You Lose!`)
-    }  if(user==="rock" && computer==="scissors"){
-        alert(`You chose ${user} and the computer chose scissors. You Win!`)
-    }  if(user==="paper" && computer==="scissors"){
-        alert(`You chose ${user} and the computer chose scissors. You Lose!`)
-    }  if(user==="paper" && computer==="rock"){
-        alert(`You chose ${user} and the computer chose rock. You Win!`)
-    }  if(user==="scissors" && computer==="paper"){
-        alert(`You chose ${user} and the computer chose paper. You Win!`)
-    }  if(user==="scissors" && computer==="rock"){
-        alert(`You chose ${user} and the computer chose rock. You Lose!`)
-    }
-}
+            if(user===computer){
+                        alert("Tie Game. You chose the same as the computer.")
+                    }else if(user==="rock" && computer ==="paper"){
+                        alert(`You chose ${user} and the computer chose ${computer}. You Lose!`)
+                    } else if(user==="rock" && computer==="scissors"){
+                        alert(`You chose ${user} and the computer chose ${computer}. You Win!`)
+                    } else if(user==="paper" && computer==="scissors"){
+                        alert(`You chose ${user} and the computer chose ${computer}. You Lose!`)
+                    } else if(user==="paper" && computer==="rock"){
+                        alert(`You chose ${user} and the computer chose ${computer}. You Win!`)
+                    } else if(user==="scissors" && computer ==="paper"){
+                        alert(`You chose ${user} and the computer chose ${computer}. You Win!`)
+                    }  else{(user==="scissors" && computer==="rock")
+                        alert(`You chose ${user} and the computer chose ${computer}. You Lose!`)
+                    }
+                }
 rps()
